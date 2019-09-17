bd1657725fab825fb6817fe2c1d1bf4154eb8e6a
Thomas S Hatch <thatch45@gmail.com>
2019-09-17 11:58:44 -0600

    a little more cleanup and I re-verified the readme docs

--- modules/legion.py
+++ modules/legion.py
@@ -13,30 +13,21 @@ from salt.exceptions import (
 
 # Import third party libs
 try:
-    from M2Crypto import RSA, EVP, BIO
+    from M2Crypto import RSA
     HAS_M2 = True
 except ImportError:
     HAS_M2 = False
 
 if not HAS_M2:
     try:
-        from Cryptodome.Cipher import AES, PKCS1_OAEP
-#        from Cryptodome.Hash import SHA
-#        from Cryptodome.PublicKey import RSA
-#        from Cryptodome.Signature import PKCS1_v1_5
-#        import Cryptodome.Random  # pylint: disable=W0611
+        from Cryptodome.Cipher import PKCS1_OAEP
         HAS_CDOME = True
     except ImportError:
         HAS_CDOME = False
 
 if not HAS_M2 and not HAS_CDOME:
     try:
-        from Crypto.Cipher import AES, PKCS1_OAEP
-#        from Crypto.Hash import SHA
-#        from Crypto.PublicKey import RSA
-#        from Crypto.Signature import PKCS1_v1_5
-        # let this be imported, if possible
-#        import Crypto.Random  # pylint: disable=W0611
+        from Crypto.Cipher import PKCS1_OAEP
     except ImportError:
         # No need for crypt in local mode
         pass
